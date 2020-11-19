9fa6ab4fe908237eb7551581e23e0dcd89b2dd7a
marchalscott <marchalscott@gmail.com>
2020-11-18 20:42:39 -0700

    added the form component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <Header />
+  <Header :totalIncome="state.totalIncome" />
+  <Form />
 </template>
 
 <script>
+import { reactive, computed } from 'vue';
 import Header from './components/Header';
+import Form from './components/Form';
 
 export default {
   setup() {
+    const state = reactive({
+      income: [],
+      totalIncome: computed(() => {
+        let temp = 0;
+        
+        if (state.income.length > 0) {
+          for (let i = 0; i < state.income.length; i++) {
+            temp += state.income[i].value;
+          }
+        }
+
+        return temp;
+      })
+    });
+
    return {
-     Header
+     Header,
+     Form,
+     state
    }
   }
 }
